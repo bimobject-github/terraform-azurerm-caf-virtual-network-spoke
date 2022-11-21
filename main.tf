4ae0e0b9c31036a49e1276ac793da16acc9467f7
Csaba Gál <csaba.gal@ad.bimobject.com>
2022-11-22 00:14:41 +0100

    --ammend

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ locals {
     privatelink_database_windows_net = "privatelink.database.windows.net",
     privatelink_documents_azure_com = "privatelink.documents.azure.com"
   }
-  private_dns_zone_names = var.private_dns_zone_enabled ? default_private_dns_zone_names : {}
+ 
 }
 
 
@@ -145,7 +145,7 @@ resource "azurerm_subnet_route_table_association" "rtassoc" {
 #---------------------------------------------
 resource "azurerm_private_dns_zone_virtual_network_link" "dzvlink" {
   provider              = azurerm.hub
-  foreach               = var.private_dns_zone_names 
+  for_each               = var.private_dns_zone_enabled ? default_private_dns_zone_names : {}
   name                  = lower("vnl-${azurerm_virtual_network.vnet.name}")
   resource_group_name   = var.private_dns_zone_resource_group_name
   virtual_network_id    = azurerm_virtual_network.vnet.id
